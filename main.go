97fcf3d8438841049da65b2fe99c1c4ff92a9e06
154pinkchairs <mholub@tutanota.com>
2022-12-22 14:28:19 +0100

    create initial structure

--- main.go
+++ main.go
@@ -1,18 +1,11 @@
 package main
 
 import (
-<<<<<<< HEAD
-=======
-	"fmt"
-
->>>>>>> 7b1bb17 (update go.mod)
 	"github.com/asynkron/protoactor-go/actor"
 	log "github.com/sirupsen/logrus"
 )
 
-<<<<<<< HEAD
-//building an actor model based firewall's main file
-//
+// building an actor model based firewall's main file
 type Firewall struct {
 }
 
@@ -27,26 +20,4 @@ func (state *Firewall) Receive(context actor.Context) {
 	case *actor.Restarting:
 		log.Info("Firewall restarting")
 	}
-=======
-func main() {
-	system := actor.NewActorSystem()
-	defer system.Shutdown()
-
-	//Set up actor to receive msgs from submodules
-	props := actor.PropsFromFunc(func(context actor.Context) {
-		switch msg := context.Message().(type) {
-		case string:
-			fmt.Println(msg)
-		default:
-			log.Printf("received unknown message: %v", msg)
-		}
-	})
-	//create actor from props
-	receiver := system.Root.Spawn(props)
-
-	//TODO: set up submods to send messages to actor
-
-	//wait for messages
-	system.AwaitTermination()
->>>>>>> 7b1bb17 (update go.mod)
 }
